6f691b46a7384da2c049924a9af497324b13c75c
Paulina Ruta <paulina.ruta@outlook.com>
2023-05-14 19:57:46 +0200

    Add container

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -5,10 +5,9 @@ const List = () => {
     return (
         <div className={styles.List}>
             <header className={styles.header}>
-                <h2 className={styles.title}> Things to do
-                    <span>soon!</span></h2>
-                <p className={styles.description}>Interesting things I want to check out</p>
+                <h2 className={styles.title}> Things to do<span>soon!</span></h2>
             </header>
+            <p className={styles.description}>Interesting things I want to check out</p>
             <section className={styles.columns}>
                 <Column title="Books" icon="book" />
                 <Column title="Movies" icon="film" />
