f319c5573c33f95d01526e8bb32808d1d9b4ed3c
Paulina Ruta <paulina.ruta@outlook.com>
2023-06-08 21:50:09 +0200

    Add Lists

--- src/components/List/List.js
+++ src/components/List/List.js
@@ -1,12 +1,18 @@
 import styles from './List.module.scss';
 import Column from '../Column/Column.js';
 import ColumnForm from '../ColumnForm/ColumnForm.js';
+import SearchForm from '../SearchForm/SearchForm.js';
 import { useSelector } from 'react-redux';
-import { getAllColumns, getListById } from '../../redux/store';
+import { getColumnsByList, getListById } from '../../redux/store';
+import { useParams } from 'react-router';
+import { Navigate } from 'react-router-dom';
 
 const List = () => {
 
-    const listData = useSelector(state => getListById(state, 1));
+    const { listId } = useParams();
+    const columns = useSelector(state => getColumnsByList(state, listId));
+    const listData = useSelector(state => getListById(state, listId));
+    if (!listData) return <Navigate to='/' />;
 
     return (
         <div className={styles.list}>
@@ -14,6 +20,7 @@ const List = () => {
                 <h2 className={styles.title}>{listData.title}<span>soon!</span></h2>
             </header>
             <p className={styles.description}>{listData.description}</p>
+            <SearchForm />
             <section className={styles.columns}>
                 {columns.map(column =>
                     <Column
@@ -21,7 +28,7 @@ const List = () => {
                         {...column} />
                 )}
             </section>
-            <ColumnForm />
+            <ColumnForm listId={listId} />
         </div>
     );
 };
