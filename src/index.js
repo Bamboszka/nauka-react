745141141d2e6ff601b9d52f1e2e3ea72d81162f
Paulina Ruta <paulina.ruta@outlook.com>
2023-06-08 13:23:14 +0200

    Add Route

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter } from 'react-router-dom';
 import { Provider } from 'react-redux';
 import store from './redux/store';
 import App from './App';
@@ -6,9 +7,12 @@ import './styles/normalize.scss';
 import './styles/global.scss';
 import 'font-awesome/css/font-awesome.min.css';
 
+
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <Provider store={store}>
-    <App />
-  </Provider>
+  <BrowserRouter>
+    <Provider store={store}>
+      <App />
+    </Provider>
+  </BrowserRouter>
 );
